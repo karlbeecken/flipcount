634c12dc2e8a608a8fe3e5344e225b9c84ee8b09
Karl Beecken <karl.beecken@icloud.com>
2019-03-11 15:50:12 +0100

    now https://standardjs.com/ conform

--- js/script.js
+++ js/script.js
@@ -6,33 +6,33 @@ let sum = 0;
 
 function doc_keyUp(e) {
 
-    if (e.keyCode === 40) {
-        clickleft();
-    } else if (e.keyCode === 38) {
-        clickright();
-    }
+  if (e.keyCode === 40) {
+    clickleft();
+  } else if (e.keyCode === 38) {
+    clickright();
+  }
 }
 document.addEventListener('keyup', doc_keyUp, false);
 
 
 function clickleft() {
-    l = l + 1;
-    sum = l + r;
-    document.getElementById('left').innerText = l;
-    document.getElementById('sum').innerText = sum;
+  l = l + 1;
+  sum = l + r;
+  document.getElementById('left').innerText = l;
+  document.getElementById('sum').innerText = sum;
 }
 
 function clickright() {
-    r = r + 1;
-    sum = r + l;
-    document.getElementById('right').innerText = r;
-    document.getElementById('sum').innerText = sum;
+  r = r + 1;
+  sum = r + l;
+  document.getElementById('right').innerText = r;
+  document.getElementById('sum').innerText = sum;
 }
 
 function getsum() {
-    lp = l / sum * 100;
-    rp = r / sum * 100;
-    alert('Rücken: ' + l + ' (' + lp + '%)\nUnten: '+ r + ' (' + rp + '%)\nInsgesamt: ' + sum);
-    console.log('Rücken: ' + l + ' (' + lp + '%)\nUnten: '+ r + ' (' + rp + '%)\nInsgesamt: ' + sum)
+  lp = l / sum * 100;
+  rp = r / sum * 100;
+  alert('Rücken: ' + l + ' (' + lp + '%)\nUnten: '+ r + ' (' + rp + '%)\nInsgesamt: ' + sum);
+  console.log('Rücken: ' + l + ' (' + lp + '%)\nUnten: '+ r + ' (' + rp + '%)\nInsgesamt: ' + sum)
 }
 
